c3e2f80c5329f7deea115a056929d11304dc5e74
Kevin Hanson <hansonks@gmail.com>
2017-02-15 20:58:59 -0500

    Add files via upload

---
+++ pay-calculator.py
@@ -0,0 +1,24 @@
+# prompt for hours and rate to compute pay with overtime calculation
+# catching exceptions with try and except
+# updated with a function
+
+def computepay(hours, rate):
+    overtime = hours % 40
+    if hours > 40:
+        pay = (40 * rate) + (overtime * rate * 1.5)
+        return pay
+    else
+        pay = hours * rate
+        return pay
+
+try:
+    hours = raw_input('How many hours did you work?')
+    hours = float(hours)
+    rate = raw_input('How much did you get payed each hour?')
+    rate = float(rate)
+except:
+    print 'Error, please enter numeric input'
+
+pay = computepay(hours, rate)
+
+print 'Pay:', pay
